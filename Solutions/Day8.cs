a392c888b3121ef268a341bc438d0fdae4628e62
ronhaland <ronnyhaland@gmail.com>
2024-12-08 12:40:18 +0100

    Day 8 ⭐⭐

--- Solutions/Day8.cs
+++ Solutions/Day8.cs
@@ -22,22 +22,21 @@ internal sealed class Day8 : Solution
         {
             var newAntiNodes = frequency.Value.SelectMany((a, i) =>
             {
-                List<(int, int)> targetAntennas = [.. frequency.Value[..i], .. frequency.Value[(i + 1)..]];
+                List<(int x, int y)> targetAntennas = [.. frequency.Value[..i], .. frequency.Value[(i + 1)..]];
                 return targetAntennas.SelectMany((t, ti) =>
                 {
-                    var diff = (t.Item1 - a.x, t.Item2 - a.y);
-                    List<(int, int)> newAntiNodes = [(t.Item1 + diff.Item1, t.Item2 + diff.Item2), (a.x - diff.Item1, a.y - diff.Item2)];
+                    (int x, int y) diff = (t.x - a.x, t.y - a.y);
+                    List<(int x, int y)> newAntiNodes = [(t.x + diff.x, t.y + diff.y), (a.x - diff.x, a.y - diff.y)];
                     return newAntiNodes;
                 });
             });
 
             foreach (var node in newAntiNodes)
             {
-                if (IsInBounds(node.Item1, node.Item2))
+                if (IsInBounds(node.x, node.y))
                     antiNodes.Add(node);
             }
         }
-
 
         return antiNodes.Count.ToString();
     }
@@ -52,6 +51,47 @@ internal sealed class Day8 : Solution
 
     internal override string Part2()
     {
-        return "";
+        HashSet<(int, int)> antiNodes = [];
+
+        var frequencies = Input.Where(c => !invalidChars.Contains(c)).GroupBy(c => c).Distinct();
+        var antennas = InputLines.SelectMany((l, y) => l.Select((c, x) => (x, y, c))).Where(c => !invalidChars.Contains(c.c)).GroupBy(c => c.c).ToDictionary(k => k.Key, v => v.Select(c => (c.x, c.y)).ToList());
+
+        foreach (var frequency in antennas)
+        {
+            var newAntiNodes = frequency.Value.SelectMany((a, i) =>
+            {
+                List<(int x, int y)> targetAntennas = [.. frequency.Value[..i], .. frequency.Value[(i + 1)..]];
+                return targetAntennas.SelectMany((t, ti) =>
+                {
+                    (int x, int y) diff = (t.x - a.x, t.y - a.y);
+                    List<(int x, int y)> newAntiNodes = [..AddNodes(t, diff, false), ..AddNodes(a, diff, true)];
+                    return newAntiNodes;
+                });
+            });
+
+            foreach (var node in newAntiNodes)
+            {
+                if (IsInBounds(node.x, node.y))
+                    antiNodes.Add(node);
+            }
+        }
+
+        return antiNodes.Count.ToString();
+    }
+
+    internal List<(int x, int y)> AddNodes((int x, int y) start, (int x, int y) diff, bool increase)
+    {
+        var result = new List<(int x, int y)>();
+        (int x, int y) next = increase ? 
+            (start.x + diff.x, start.y + diff.y) : 
+            (start.x - diff.x, start.y - diff.y);
+        while (IsInBounds(next.x, next.y))
+        {
+            result.Add((next.x, next.y));
+            next = increase ?
+            (next.x + diff.x, next.y + diff.y) :
+            (next.x - diff.x, next.y - diff.y);
+        }
+        return result;
     }
 }
